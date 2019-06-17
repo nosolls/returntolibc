dfb35e19273d67268796361d7a20c24f9392b684
Noah Oller Smith <nollersm@purdue.edu>
2019-06-17 13:17:14 -0400

    Fixed code / typos

--- client/getbinsh.c
+++ client/getbinsh.c
@@ -1,10 +1,10 @@
 /* getbinsh.c */
 
 #include <stdio.h>
-#include <stdlb.h>
+#include <stdlib.h>
 
 void main() {
 	char* shell = getenv("MYSHELL");
 	if (shell)
-		printf("%x\n", (unsigned int)shell);
+		printf("0x%x\n", (unsigned int)shell);
 }
